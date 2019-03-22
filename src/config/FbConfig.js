620cceadaa6cd5ec51fb0fcbafc05499d82ff147
Muhammad Usama <37137366+HMUsama@users.noreply.github.com>
2019-03-22 16:05:20 +0500

    Update FbConfig.js

--- src/config/FbConfig.js
+++ src/config/FbConfig.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var config = {
-    apiKey: "AIzaSyD5mGCTsyUKju2torq81NtnCnADD1WrKlM",
-    authDomain: "hackathonfinal-4b942.firebaseapp.com",
-    databaseURL: "https://hackathonfinal-4b942.firebaseio.com",
-    projectId: "hackathonfinal-4b942",
-    storageBucket: "hackathonfinal-4b942.appspot.com",
-    messagingSenderId: "509247192177"
+    apiKey: "************************",
+    authDomain: "********************",
+    databaseURL: "******************",
+    projectId: "******************",
+    storageBucket: ""***************",
+    messagingSenderId: "***************"
   };
   firebase.initializeApp(config);
 
